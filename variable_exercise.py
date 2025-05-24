504b0fd3dc7905740fc1a19d2236f964a3a95646
itsNawar <nawar.noushin784@gmail.com>
2025-05-24 10:32:58 +0600

    exercise

---
+++ variable_exercise.py
@@ -0,0 +1,17 @@
+#Create two variables. 
+#One to store your birth year and another one to store current year. 
+#Now calculate your age using these two variables
+
+birth_year=2005
+current_year=2025
+age = current_year-birth_year
+print(age)  #ans.20
+
+#Store your first, middle and last name in three 
+#different variables and then print your full name using these variables
+
+first="Noushin"
+middle="Nawar"
+last="Afra"
+full_name= first+ " "+ middle+" "+last
+print("My full name is ",full_name) #ans.My full name is  Noushin Nawar Afra
